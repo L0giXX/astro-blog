c6a918ea8cc76001f84c98f8033363f7c15c125f
L0giXX <marc.mueller04@gmail.com>
2023-09-02 02:05:21 +0200

    adding mdx config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,17 @@
 import { defineConfig } from "astro/config";
-
 import vercel from "@astrojs/vercel/serverless";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: vercel(),
+  integrations: [
+    mdx({
+      syntaxHighlight: "shiki",
+      shikiConfig: {
+        theme: "nord",
+      },
+    }),
+  ],
 });
